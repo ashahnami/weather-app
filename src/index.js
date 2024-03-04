3bd8e07ed80fa93adb73f3d4c678dad683b343ae
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-04 10:29:41 +0000

    Add browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import InfoPage from './pages/InfoPage';
+import LandingPage from './pages/LandingPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "weather",
+    element: <InfoPage />
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
