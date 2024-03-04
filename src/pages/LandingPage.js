12797015d6464bb5c74f7526215fdd5908f29cfd
suraj shah <ec22894@qmul.ac.uk>
2024-03-04 10:47:56 +0000

    landing page layout

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div>
+        <h1>
+            Farming Weather
+        </h1>
+        <h2>
+            Weather forecasts and information
+        </h2>
+
+        <p>Search for your location below</p>
+        <button>Search</button>
+    </div>
   )
 }
 
