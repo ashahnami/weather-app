7022d30a8442d5a1af2881c8238b9c4331a77715
Karishan <ec22899@qmul.ac.uk>
2024-03-06 16:47:18 +0000

    div elements implemented for InfoPage

--- src/pages/InfoPage.js
+++ src/pages/InfoPage.js
@@ -36,6 +36,60 @@ function InfoPage() {
         </div>
       </div>
 
+      <div class = "flexbox-container">
+        
+
+          <div class = "flexbox-item-bottom-row">
+
+          <div class = "flexbox-container">
+              <div class = "flexbox-item">
+              </div> 
+
+            </div>
+
+            <div class = "flexbox-container">
+
+
+              <div class = "flexbox-item">
+              </div>
+              <div class = "flexbox-item">
+              </div>
+
+            </div>
+
+            
+          </div>
+
+          <div class = "flexbox-item-bottom-row">
+
+            <div class = "flexbox-container">
+              <div class = "flexbox-item">
+              </div> 
+
+            </div>
+            
+          </div>
+
+          <div class = "flexbox-item-bottom-row">
+
+          <div class = "flexbox-container">
+
+                <div class = "flexbox-item">
+                </div> 
+          </div>
+          
+          <div class = "flexbox-container">
+          <div class = "flexbox-item">
+                </div> 
+
+          </div>
+                
+
+
+          </div>
+
+      </div>
+
     </div>
 
   )
