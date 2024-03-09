cb82b3303166c76bc504f7f3fe524d451f5b90e0
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-09 00:36:56 +0000

    Add Hourly.js and hourly.css

---
+++ src/components/Hourly.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import '../assets/hourly.css';
+
+function Hourly() {
+  return (
+    <div className='hourly'>
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+
+      <div>5</div>
+    </div>
+  )
+}
+
+export default Hourly
