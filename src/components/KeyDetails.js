d7725550829754a8865c86e36be944b2e4276154
Suraj Shah <s.shah@se22.qmul.ac.uk>
2024-03-12 15:33:37 +0000

    key weather details

--- src/components/KeyDetails.js
+++ src/components/KeyDetails.js
@@ -1,15 +1,43 @@
-import React from 'react';
-
-import '../assets/keydetails.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../assets/keydetails.css";
 
 function KeyDetails({ place }) {
+  const [details, setDetails] = useState(null);
+
+  /* just test coordinates for London */
+  let lat = 51.5072;
+  let lon = 0.1276;
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+    );
+    setDetails(response.data);
+    console.log(response.data);
+  };
+  useEffect(() => {
+    fetchData();
+  });
+
   return (
-    <div className='keydetails'>
-      <div className='location'>{place}</div>
-      <div className='temperature'>5°<span className='range'>±0.5</span></div>
-      <div className='alt-temperature'>Feels like 3°</div>
+    <div className="keydetails">
+      {details ? (
+        <>
+          <div className="location">{place}</div>
+          <div className="temperature">
+            {Math.round(details.main.temp)}°<span className="range">±0.5</span>
+          </div>
+          <div className="alt-temperature">
+            <p>
+              Feels like <span>{Math.round(details.main.feels_like)}°C</span>
+            </p>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default KeyDetails;
