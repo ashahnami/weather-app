e798f9443bbb9138e03b651ec4d961e6735d000b
Karishan <ec22899@qmul.ac.uk>
2024-03-13 14:29:13 +0000

    added Hourly weather component

--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -1,74 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 import '../assets/hourly.css';
 
 function Hourly() {
-  return (
-    <div className='hourly'>
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+  const [forecast, setForecast] = useState([]);
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+  let lat=51.5072;
+  let lon=0.1276;
+  /* number of days forecast to receive */
+  let cnt=8;
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+  useEffect(() => {
+    axios
+    .get(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(function (response) {
+        setForecast(response.data.list);
+        console.log(response.data.list);
+    });
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+    
+  }, [])
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+    return (
+      
+      <div className='hourly'>
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+         {forecast.slice(0, 8)?.map((hourlyData, index) => {
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
-      </div>
+          return(
 
-      <div>
-        <div className='time'>10:00</div>
-        <div className='icon'>icon</div>
-        <div className='temperature'>5°<span>±0.5</span></div>
-        <div className='rainfall'>30%</div>
+          
+
+           <div key={index}>
+            
+          
+            <div>
+              <div className='time'>{new Date(hourlyData.dt * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</div>
+              <div className='icon'><img className='icon' src={`https://openweathermap.org/img/wn/${hourlyData.weather[0].icon}@2x.png`}></img></div>
+              <div className='temperature'>{Math.round(hourlyData.main.temp)}°<span>±0.5</span></div>
+              <div className='rainfall'>{hourlyData.pop*100}%</div>
+            </div>
+            
+          
+          </div>
+
+          )
+          
+
+    })}
+
       </div>
-    </div>
-  )
+    )
 }
 
-export default Hourly
+export default Hourly;
