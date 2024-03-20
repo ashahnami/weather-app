6610cb1c228657e27ecb0b2686a9d4a9c19d2e6e
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-20 19:16:31 +0000

    Update bookmarks

--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
- import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
 
 import { LocationContext } from '../context';
 import '../assets/hourly.css';
@@ -11,22 +11,22 @@ function Hourly() {
 
   const fetchData = () => {
     axios
-    .get(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${location.lat}&lon=${location.lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-    .then(function (response) {
+      .get(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${location.lat}&lon=${location.lon}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+      .then(function (response) {
         setForecast(response.data.list);
-    });
+      });
   }
 
   useEffect(() => {
-    if (location) fetchData(); 
+    if (location) fetchData();
   }, [location])
 
-    return (
-      <div className='hourly'>
-        {forecast.slice(0, 8)?.map((hourlyData, index) => {
+  return (
+    <div className='hourly'>
+      {forecast.slice(0, 8)?.map((hourlyData, index) => {
         return (
-          <div className='row' key={index}>
-            <div className='time'>{new Date(hourlyData.dt * 1000).toLocaleTimeString('en-UK', {hour: '2-digit', minute:'2-digit', hour12: false})}</div>
+          <div className='col' key={index}>
+            <div className='time'>{new Date(hourlyData.dt * 1000).toLocaleTimeString('en-UK', { hour: '2-digit', minute: '2-digit', hour12: false })}</div>
 
             <div className='icon'><img className='icon' src={`https://openweathermap.org/img/wn/${hourlyData.weather[0].icon}@2x.png`}></img></div>
 
@@ -35,17 +35,17 @@ function Hourly() {
                 ±{((hourlyData.main.temp_max - hourlyData.main.temp_min) / 2).toFixed(1)}
               </span>
             </div>
-            
-            <div className='rainfall'>
-              <WaterDropIcon className='icon'/>
 
-              <div className='value'>{Math.round(hourlyData.pop*100)}%</div>  
+            <div className='rainfall'>
+              <WaterDropIcon className='icon' />
+
+              <div className='value'>{Math.round(hourlyData.pop * 100)}%</div>
             </div>
           </div>
         )
-        })}
-      </div>
-    )
+      })}
+    </div>
+  )
 }
 
 export default Hourly;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 import SearchBar from "../components/SearchBar";
 import "../assets/landing.css";
@@ -9,6 +11,7 @@ import axios from "axios";
 function LandingPage() {
   const [location, setLocation] = useContext(LocationContext);
   const [details, setDetails] = useState();
+  const [hovered, setHovered] = useState(false);
 
   let navigate = useNavigate();
 
@@ -33,6 +36,28 @@ function LandingPage() {
     }
   };
 
+  const removeBookmark = (index) => {
+    let locations = JSON.parse(localStorage.getItem("locations"));
+
+    if (locations === null) return;
+
+    try {
+      locations.data.splice(index, 1);
+    } catch (error) {
+      console.log(error);
+    }
+
+    localStorage.setItem("locations", JSON.stringify(locations));
+    return false;
+  };
+
+  const handleClick = (bookmark) => {
+    if (!hovered) {
+      setLocation(bookmark);
+      navigate("/weather");
+    }
+  }
+
   useEffect(() => {
     if (location) fetchWeatherForBookmarks();
   }, [location]);
@@ -49,32 +74,35 @@ function LandingPage() {
             ? JSON.parse(localStorage.getItem("locations")).data.map(
               (bookmark, index) => {
                 return (
-                  <div
-                    className="bookmark"
-                    onClick={() => {
-                      setLocation(bookmark);
-                      navigate("/weather");
-                    }}
-                    key={index}
-                  >
+                  <div className="row" key={index}>
+                    <div
+                      className="bookmark"
+                      onClick={() => { handleClick(bookmark) }}
+                      key={index}
+                    >
 
-                    {details ? (
-                      <>
-                        <div>
-                          <div>{bookmark.place}, {bookmark.country}</div>
-                          <div className="description">
-                            {details[index].weather[0].description.charAt(0).toUpperCase() +
-                              details[index].weather[0].description.slice(1)}
+                      {details ? (
+                        <>
+                          <div>
+                            <div>{bookmark.place}, {bookmark.country}</div>
+                            <div className="description">
+                              {details[index].weather[0].description.charAt(0).toUpperCase() +
+                                details[index].weather[0].description.slice(1)}
+                            </div>
                           </div>
-                        </div>
 
-                        <div>
-                          <div className="max-temp">{Math.round(details[index].main.temp_max)}°</div>
-                          <div className="min-temp">{Math.round(details[index].main.temp_min)}°</div>
-                        </div>
-                      </>
-                    ) : null}
-
+                          <div>
+                            <div className="max-temp">{Math.round(details[index].main.temp_max)}°</div>
+                            <div className="min-temp">{Math.round(details[index].main.temp_min)}°</div>
+                          </div>
+                        </>
+                      ) : null}
+                    </div>
+                    {!hovered ?
+                      <BookmarkIcon className="bookmark-icon" onMouseEnter={() => setHovered(true)}>remove</BookmarkIcon>
+                      :
+                      <BookmarkBorderIcon className="bookmark-icon" onMouseLeave={() => setHovered(false)} onClick={() => { removeBookmark(index); navigate('/') }} />
+                    }
                   </div>
                 );
               }
